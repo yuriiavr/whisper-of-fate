2bd3f8569064501f15f55d389e48c114fc4d706c
yuriiavr <94492796+yuriiavr@users.noreply.github.com>
2026-03-30 01:28:12 +0300

    change router

--- whisper-of-fate/src/App.tsx
+++ whisper-of-fate/src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import TarotPage from './pages/TarotPage';
 import NatalPage from './pages/NatalPage';
 
